1e676c6a1090a0f815c3c312ee6fb870222b0e18
Peter Isaiah <peterisaiah4fun@gmail.com>
2021-09-19 22:15:21 +0100

    slide

--- src/component/Slider.jsx
+++ src/component/Slider.jsx
@@ -1,11 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import { ArrowLeftOutlined, ArrowRightOutlined } from "@material-ui/icons";
+import { sliderItems } from "../data";
 
 const Container = styled.div`
   width: 100%;
   height: 100vh;
   display: flex;
+  overflow: hidden;
 `;
 
 const Arrow = styled.div`
@@ -24,19 +26,85 @@ const Arrow = styled.div`
   right: ${(props) => props.direction === "right" && "10px"};
   cursor: pointer;
   opacity: 0.5;
+  z-index: 200;
 `;
 
 const Wrapper = styled.div`
-  hieght: 100%;
+  height: 100%;
+  display: flex;
+  transition: all 1s ease;
+  transform: translateX(${(props) => props.slideIndex * -100}vw);
+  text-transform: uppercase;
+`;
+
+const Slide = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  background-color: #${(props) => props.bg};
+`;
+const ImageContainer = styled.div`
+  height: 100%;
+  flex: 1;
+`;
+
+const Image = styled.img`
+  height: 80%;
+`;
+
+const InfoContainer = styled.div`
+  flex: 1;
+  padding: 50px;
+`;
+
+const Title = styled.h1`
+  font-size: 70px;
+`;
+const Desc = styled.p`
+  margin: 50px 0;
+  font-size: 20px;
+  font-weight: 500;
+  letter-spacing: 3px;
+`;
+const Button = styled.button`
+  padding: 10px;
+  font-size: 20px;
+  background-color: transparent;
+  cursor: pointer;
 `;
 
 const Slider = () => {
+  const [slideIndex, setSlideIndex] = useState(0);
+  const handleClick = (direction) => {
+    if (direction === "left") {
+      setSlideIndex(slideIndex > 0 ? slideIndex - 1 : sliderItems.length - 1);
+    } else {
+      setSlideIndex(slideIndex < sliderItems.length - 1 ? slideIndex + 1 : 0);
+    }
+  };
   return (
     <Container>
-      <Arrow direction="left">
+      <Arrow direction="left" onClick={() => handleClick("left")}>
         <ArrowLeftOutlined />
       </Arrow>
-      <Arrow direction="right">
+      <Wrapper slideIndex={slideIndex}>
+        {sliderItems.map((item) => {
+          return (
+            <Slide key={item.id} bg={item.bg}>
+              <ImageContainer>
+                <Image src={item.img} />
+              </ImageContainer>
+              <InfoContainer>
+                <Title>{item.title}</Title>
+                <Desc>{item.desc}</Desc>
+                <Button>SHOW NOW</Button>
+              </InfoContainer>
+            </Slide>
+          );
+        })}
+      </Wrapper>
+      <Arrow direction="right" onClick={() => handleClick("right")}>
         <ArrowRightOutlined />
       </Arrow>
     </Container>
---
+++ src/data.js
@@ -0,0 +1,41 @@
+export const sliderItems = [
+  {
+    id: 1,
+    img: "https://i.ibb.co/XsdmR2c/1.png",
+    title: "summer sale",
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% ON NEW ARRIVALS.",
+    bg: "f5fafd",
+  },
+  {
+    id: 2,
+    img: "https://i.ibb.co/DG69bQ4/2.png",
+    title: "Autum colection",
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% ON NEW ARRIVALS.",
+    bg: "fcf1ed",
+  },
+  {
+    id: 3,
+    img: "https://i.ibb.co/cXFnLLV/1.png",
+    title: "winter sale",
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% ON NEW ARRIVALS.",
+    bg: "fbf0f4",
+  },
+];
+
+export const categories = [
+  {
+    id: 1,
+    img: "https://image.pexels.com/photos//pexels-photo-.jpg",
+    title: "Shirt shirt",
+  },
+  {
+    id: 2,
+    img: "https://image.pexels.com/photos//pexels-photo-.jpg",
+    title: "loungewear love",
+  },
+  {
+    id: 3,
+    img: "https://image.pexels.com/photos//pexels-photo-.jpg",
+    title: "light jackets",
+  },
+];
