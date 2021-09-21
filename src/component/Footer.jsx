5e323437438665d067ec0e99ca889af991753fe5
Peter Isaiah <peterisaiah4fun@gmail.com>
2021-09-21 22:46:59 +0100

    footer

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -1,6 +1,14 @@
 import React from "react";
 import styled from "styled-components";
-import { Facebook, Instagram, Pinterest, Twitter } from "@material-ui/icons";
+import {
+  Facebook,
+  Instagram,
+  MailOutline,
+  Phone,
+  Pinterest,
+  Room,
+  Twitter,
+} from "@material-ui/icons";
 
 const Container = styled.div`
   display: flex;
@@ -14,16 +22,59 @@ const Left = styled.div`
 
 const Logo = styled.h1``;
 
-const Description = styled.p``;
+const Description = styled.p`
+  margin: 20px 0px;
+`;
 
-const SocialMediaContainer = styled.div``;
-const SocialIcon = styled.div``;
+const SocialMediaContainer = styled.div`
+  display: flex;
+  align-item: center;
+`;
+const SocialIcon = styled.div`
+  width: 40px;
+  height: 40px;
+  border-radius: 20%;
+  color: white;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: #${(props) => props.color};
+  margin-right: 20px;
+`;
+
+const Center = styled.div`
+  flex: 1;
+  padding: 20px;
+`;
+
+const Title = styled.h3`
+  margin-bottom: 30px;
+`;
+
+const List = styled.ul`
+  list-style: none;
+  display: flex;
+  flex-wrap: wrap;
+`;
+
+const ListItem = styled.li`
+  width: 50%;
+  margin-bottom: 10px;
+`;
 
 const Right = styled.div`
   flex: 1;
+  padding: 20px;
 `;
-const Center = styled.div`
-  flex: 1;
+
+const ContactItem = styled.div`
+  margin-bottom: 20px;
+  display: flex;
+  align-items: center;
+`;
+
+const Payment = styled.img`
+  width: 50%;
 `;
 
 const Footer = () => {
@@ -36,22 +87,47 @@ const Footer = () => {
           obcaecati ducimus fuga ipsa enim explicabo? Dicta officia, id debitis
         </Description>
         <SocialMediaContainer>
-          <SocialIcon>
+          <SocialIcon color="3b5999">
             <Facebook />
           </SocialIcon>
-          <SocialIcon>
+          <SocialIcon color="e4405f">
             <Instagram />
           </SocialIcon>
-          <SocialIcon>
+          <SocialIcon color="55acee">
             <Twitter />
           </SocialIcon>
-          <SocialIcon>
+          <SocialIcon color="e60023">
             <Pinterest />
           </SocialIcon>
         </SocialMediaContainer>
       </Left>
-      <Right></Right>
-      <Center></Center>
+      <Center>
+        <Title>Useful links</Title>
+        <List>
+          <ListItem>Home</ListItem>
+          <ListItem>Cart</ListItem>
+          <ListItem>Man Fashion</ListItem>
+          <ListItem>Woman Fashion</ListItem>
+          <ListItem>Accessories</ListItem>
+          <ListItem>My Account</ListItem>
+          <ListItem>Order tracking</ListItem>
+          <ListItem>Wishlist</ListItem>
+          <ListItem>Term</ListItem>
+        </List>
+      </Center>
+      <Right>
+        <Title>Contact</Title>
+        <ContactItem>
+          <Room style={{ marginRight: "10px" }} /> No.33 Arikenbe street
+        </ContactItem>
+        <ContactItem>
+          <Phone style={{ marginRight: "10px" }} /> +234 810 466 3471
+        </ContactItem>
+        <ContactItem>
+          <MailOutline style={{ marginRight: "10px" }} /> peterisaiah@gmail.com
+        </ContactItem>
+        <Payment src="https://i.ibb.co/Qfvn4z6/payment.png" />
+      </Right>
     </Container>
   );
 };
