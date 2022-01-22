b31e56f1a5dcfb0756b5b3c095d0b060718a7c40
Peter Isaiah <peterisaiah4fun@gmail.com>
2022-01-22 16:45:59 +0100

    more change

--- src/requestMethod.js
+++ src/requestMethod.js
@@ -1,8 +1,10 @@
 import axios from "axios";
 
 const BASE_URL = "http://localhost:5000";
-const TOKEN = JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user)
-  .currentUser.token;
+const TOKEN =
+  " " ||
+  JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user).currentUser
+    .token;
 
 console.log(console.log(TOKEN));
 
